a1b97cad7c8c1c3bc6b0e2be99c99d6624461c97
Daniyar <iamdanishberg@gmail.com>
2024-02-23 11:29:49 +0000

    test

--- Backend.py
+++ Backend.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'your_database_connection_string'
 db = SQLAlchemy(app)
 
-#testtesttest
 # User model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
